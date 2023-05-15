06eb116ba00f71a69cdf3a0536433fe2905b5d96
Nianoute <enzo.angot@gmail.com>
2023-05-15 12:31:57 +0200

    [ADD]/create post style + better post design

--- src/app/components/post/DesignPost.jsx
+++ src/app/components/post/DesignPost.jsx
@@ -1,31 +1,68 @@
 import * as React from 'react';
 
 export default function GetAllPostDesign({post}) {
+    let pricePercent = 0;
+    if(post.priceInit){
+      pricePercent = Math.round((post.priceInit - post.priceNow) / post.priceInit * 100);
+    }
     return (
         <div className='post'>
           <div className='postInfos'>
             <div className='postInfosPrimary'>
-              <div className='postAvatar'>
-                <img src=".img/post/default_imagePost.png" alt='avatar' />
+              <div className='postLogo'>
+                <img src="logo192.png" alt='avatar' />
               </div>
-              <div className='postTitle'>
-                <h2>{post.title}</h2>
+              <div className='postPrixTitle'>
+                <div className='postTitle'>
+                  <h2>{post.title}</h2>
+                </div>
+                <div className='postPrix'>
+                  <p className='priceNow'>{post.priceNow}€</p>
+                  {post.priceInit?
+                    <>
+                      <p className='priceInit'>{post.priceInit}€</p>
+                      <p className='pricePercent'>{pricePercent}%</p>
+                    </>
+                  : <></>}
+                </div>
               </div>
             </div>
 
             <div className='postInfosSecondary'>
-              <p>{post.description}</p>
+              <p>Description: {post.description}</p>
             </div>
 
             <div className='postInfosTertiary'>
-              {post.author? 
-                <p>auteur: {post.author.userName}, id: {post.author.id}</p>
-              : <></>}
-              {post.categories?.map((category, index)=> {
-                return(
-                  <p key={category.id}>catégorie {index + 1}: {category.name}</p>
-                )
-              })}
+              <div className='postAuthor'>
+                {post.author?
+                  <>
+                    <img src="logo512.png" className='postAuthorAvatar' alt='avatar' />
+                    <p>{post.author.userName}</p>
+                  </>
+                : <></>}
+              </div>
+
+              <div className='postCategories'>
+                {post.categories?
+                  <>
+                    {post.categories?.map((category)=> {
+                      return(
+                        <p key={category.id} className='postCategory'>{category.name}</p>
+                      )
+                    })}
+                  </>
+                    : <></>
+                }
+              </div>
+
+              <div className='postDates'>
+                <div className='postDate'>
+                  <p>Créé le: {post.createdAt}</p>
+                </div>
+                <div className='postDate'>
+                  <p>{post.promoDuration}</p>
+                </div>
+              </div>
             </div>
           </div>
 
--- src/app/pages/posts/CreateNewPostPage.jsx
+++ src/app/pages/posts/CreateNewPostPage.jsx
@@ -28,8 +28,11 @@ const CreateNewPost = () => {
     }, []);
 
     const [post, setPost] = useState({
+        webSite: "",
         title: "",
         description: "",
+        priceNow: 0,
+        priceInit: 0,
         category: "",
         category2: "",
     });
@@ -65,46 +68,85 @@ const CreateNewPost = () => {
       
       
     return (
-      <>
+      <div className="createPost">
         <h1>Creation d'un post</h1>
-        <form onSubmit={handleCreatePost}>
-            <label>
-                Le titre:
-                <input type="text" onChange={onChangePost} value={post.title} name="title" />
-            </label>
-            <label>
-                La description:
-                <input type="text" onChange={onChangePost} value={post.description} name="description" />
-            </label>
+        <form onSubmit={handleCreatePost} className="formCreatePost">
+            <div className="formStep">
+              <h2>Lien du site</h2>
+              <div className="oneLabel">
+                <label>
+                    <p>webSite:</p>
+                    <input type="text" onChange={onChangePost} value={post.webSite} name="webSite" className="inputForm"/>
+                </label>
+              </div>
+            </div>
 
-            <label>
-              Choix catégory
-              <select name="category" onChange={onChangePost}>
-                  <option value="">--Aucun--</option>
-                  {categories?.map((category) => (
-                      <option value={category.id} key={category.id}>--{category.name}--</option>
-                 ))}
-              </select>
-            </label>
-                  
-            
-            {post.category?  
-              <label>            
-                Choix catégory 2
-                <select name="category2" onChange={onChangePost}>
-                    <option value="">--Aucun--</option>
-                    {categories?.map((category) => (
-                        <option value={category.id} key={category.id}>--{category.name}--</option>
-                  ))}
-                </select>
+            <div className="formStep">
+              <h2>Informations du post</h2>
+              <div className="oneLabel">
+                <label>
+                    <p>Le titre:</p>
+                    <input type="text" onChange={onChangePost} value={post.title} name="title" className="inputForm"/>
+                </label>
+              </div>
+              <div className="oneLabel">
+                <label>
+                    <p>La description:</p>
+                    <input type="text" onChange={onChangePost} value={post.description} name="description" className="inputForm"/>
+                </label>
+              </div>
+            </div>
+
+            <div className="formStep">
+              <h2>Les prix</h2>
+              <div className="oneLabel">
+                <label>
+                    <p>Le prix actuel:</p>
+                    <input type="number" onChange={onChangePost} value={post.priceNow} name="priceNow" className="inputForm"/>
+                </label>
+              </div>
+              <div className="oneLabel">
+              <label>
+                  <p>Le prix initial:</p>
+                  <input type="number" onChange={onChangePost} value={post.priceInit} name="priceInit" className="inputForm"/>
               </label>
-            : <></>}
- 
-            
+              </div>
+            </div>
 
-            <input type="submit" value="Submit" />
+            <div className="formStep">
+              <h2>Les catégories</h2>
+              <div className="oneLabel">
+                <label>
+                  <p>Choix catégory</p>
+                  <select name="category" onChange={onChangePost} className="select">
+                      <option value="">--Aucun--</option>
+                      {categories?.map((category) => (
+                          <option value={category.id} key={category.id}>--{category.name}--</option>
+                    ))}
+                  </select>
+                </label>
+              </div>
+                    
+              
+              {post.category? 
+                <div className="oneLabel"> 
+                  <label>            
+                    <p>Choix catégory 2</p>
+                    <select name="category2" onChange={onChangePost} className="select">
+                        <option value="">--Aucun--</option>
+                        {categories?.map((category) => (
+                            <option value={category.id} key={category.id}>--{category.name}--</option>
+                      ))}
+                    </select>
+                  </label>
+                </div>
+              : <></>}
+            </div>
+ 
+
+            <input type="submit" value="Submit" className="primaryBouton"/>
         </form>
-      </>
+      </div>
     );
   };
   
